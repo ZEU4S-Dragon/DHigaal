32aac1341ce5bf6c93d1d08e30c0aaa443c26150
Abdalla <anddragon34@gmail.com>
2025-07-14 02:08:54 +0300

    make the card dynamic

--- main.js
+++ main.js
@@ -4,3 +4,59 @@ const menu = document.getElementById("menu");
 menuIcon.addEventListener("click",()=>{
     menu.classList.toggle("hidden")
 });
+
+// end menu responsive
+
+
+const allProduct=[
+    {
+        image:"images/lightout-01.png",
+        name:"Lights Out",
+        Author:"Navessa Allen",
+        Price:11.99
+    },
+    {
+        image:"images/Righdad-01.png",
+        name:"Rich Dad Poor Dad",
+        Author:"Robert Kiyosaki",
+        Price:34.99
+    },
+    {
+        image:"images/7habit-01.png",
+        name:"Atomic Habits",
+        Author:"James Clear",
+        Price:54.99
+    },
+    {
+        image:"images/maketoday-01.png",
+        name:"Make Today Count",
+        Author:"John C. Maxwell",
+        Price:12.99
+    },
+    {
+        image:"images/learnstatup-01.png",
+        name:"The Lean Startup",
+        Author:"Eric Ries",
+        Price:33.99
+    },
+    {
+        image:"images/art of the war.jpg",
+        name:"The Art of War",
+        Author:" Sun Tzu",
+        Price:53.99
+    }
+]
+
+
+const productCard = document.getElementById("productCard"); 
+
+allProduct.map((product)=>{
+    const ProductHtml = `<div class="shadow-lg   px-4 py-6 ">
+            <img class="w-full" src="${product.image}" alt="">
+            <h3 class="font-mlight py-1"><b>Name: <b/>${product.name}</h3>
+            <h3 class="font-medium"><b>Author: <b/>${product.Author}</h3>
+            <h3 class="font-medium py-1"><b>Author: <b/>$${product.Price}</h3>
+            <button class="bg-[#E2FE26] py-0.5 px-4 text-lg">Buy Now</button>
+          </div>`;
+          productCard.innerHTML += ProductHtml;
+});
