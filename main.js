2853d9bea3117b488b0eb26b73d759b8255f9541
Abdalla <anddragon34@gmail.com>
2025-07-14 15:50:33 +0300

    style buy page

--- main.js
+++ main.js
@@ -47,16 +47,18 @@ const allProduct=[
     }
 ]
 
+//Fetch the product
 
 const productCard = document.getElementById("productCard"); 
 
 allProduct.map((product)=>{
     const ProductHtml = `<div class="shadow-lg   px-4 py-6 ">
             <img class="w-full" src="${product.image}" alt="">
-            <h3 class="font-mlight py-1"><b>Name: <b/>${product.name}</h3>
-            <h3 class="font-medium"><b>Author: <b/>${product.Author}</h3>
-            <h3 class="font-medium py-1"><b>Author: <b/>$${product.Price}</h3>
+            <h3 class="font-light py-1"><b>Name:<b/> ${product.name}</h3>
+            <h3 class="font-light"><b>Author: <b/>${product.Author}</h3>
+            <h3 class="font-light py-1"><b>Author: <b/>$${product.Price}</h3>
             <button class="bg-[#E2FE26] py-0.5 px-4 text-lg">Buy Now</button>
           </div>`;
           productCard.innerHTML += ProductHtml;
 });
+
